beaf3667ad48be4a973529d8276fb520327d8150
KHANDELWALGROUPANDCOMPANY <KHANDELWALGROUPANDCOMPANY@GMAIL.COM>
2025-04-15 21:37:17 +0530

    Create service-worker.js

---
+++ _includes/service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = "kgc-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png",
+  // Add other important files here if needed
+];
+
+// Install event
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
